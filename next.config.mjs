fc7ce987a8076f21e5b9fdc1f2000946717c5203
MarkLesterAno <fl.marklesterano@gmail.com>
2026-06-23 15:20:55 +0800

    remove the Decap CMS

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/admin",
-        destination: "/admin/index.html",
-      },
-    ]
-  },
 }
 
 export default nextConfig
